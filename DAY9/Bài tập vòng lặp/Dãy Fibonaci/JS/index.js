0270488aed8e02d0c6ea149877e91ce820681e82
Danh <danhpharma12697@gmail.com>
2022-03-29 18:42:42 +0700

    29/03/2022
    Signed-off-by: Danh <danhpharma12697@gmail.com>

--- DAY9/Bài tập vòng lặp/Dãy Fibonaci/JS/index.js
+++ DAY9/Bài tập vòng lặp/Dãy Fibonaci/JS/index.js
@@ -1,10 +1,10 @@
-const number = parseInt(prompt('Enter the number of terms: '));
+
 let n1 = 0, n2 = 1, nextTerm;
 
-console.log('Fibonacci Series:');
+document.write('Fibonacci Series:'+'<br>');
 
-for (let i = 1; i <= number; i++) {
-    console.log(n1);
+for (let i = 1; i <= 20; i++) {
+    document.write(n1 +'<br>');
     nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
